c090f951c0c3dd57242e8b3dd8327d208fa1b62e
André Ventura <asventura@live.com>
2021-10-29 18:45:16 -0300

    Inserção dos Estudos de Módulos em JavaScript.

---
+++ javascript-modulos/README.md
@@ -0,0 +1,3 @@
+# Aprendizado JavaScript
+
+## Módulos
---
+++ javascript-modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade("André Ventura", 25));
+console.log(mostraCidade("André Ventura", "Belo Horizonte"));
+console.log(mostraHobby("André Ventura", "Tecnologia"));
